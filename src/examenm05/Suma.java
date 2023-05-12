06ae80ae4549eaa914c57787a08ec6f8fc2632f3
jeam226 <118379263+jeam226@users.noreply.github.com>
2023-05-12 10:42:18 +0200

    Add files via upload

--- src/examenm05/Suma.java
+++ src/examenm05/Suma.java
@@ -2,21 +2,10 @@ package examenm05;
 
 public class Suma {
     
-    //Sumar que retorna la suma de dos nombres enters
-
-//    public int sumaEnters(int Nombre1, int Nombre2) {
-//        return Nombre1 + Nombre2;
-//    }
-    
     // ens retorna el valor del primer número a sumar
     
     public int getNombre1(int Nombre1, int Nombre2) {
         return Nombre1;
     }
-//    
-//    // ens retorna la suma del valor absolut de dos nombres
-//    
-//    public double sumaValorAbsolut(double Nombre1, double Nombre2) {
-//        return Nombre1 + Nombre2;
-//    }
+    
 }
